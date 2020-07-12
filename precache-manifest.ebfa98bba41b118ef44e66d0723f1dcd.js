17a71278bd53596702d86814adc0a0a5ec91fb9a
Ulvi <ulvi199517@gmail.com>
2020-07-12 18:30:12 -0400

    Updates

---
+++ precache-manifest.ebfa98bba41b118ef44e66d0723f1dcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d377e107827244dc51f0163dcd04b8b",
+    "url": "/face_recognition_brain/index.html"
+  },
+  {
+    "revision": "e673a7bd8a0a32201f53",
+    "url": "/face_recognition_brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b7d59ca9dddb05f4a327",
+    "url": "/face_recognition_brain/static/css/main.566b5544.chunk.css"
+  },
+  {
+    "revision": "e673a7bd8a0a32201f53",
+    "url": "/face_recognition_brain/static/js/2.b46afa5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face_recognition_brain/static/js/2.b46afa5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d59ca9dddb05f4a327",
+    "url": "/face_recognition_brain/static/js/main.93945fe9.chunk.js"
+  },
+  {
+    "revision": "2ace170157ce010fcbbd",
+    "url": "/face_recognition_brain/static/js/runtime-main.3ffdbcea.js"
+  }
+]);
